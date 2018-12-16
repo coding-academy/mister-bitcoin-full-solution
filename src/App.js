3163fae4932baf1355619f6c75f9dc6783c24841
moshe-pinhasi <pi.moshe@gmail.com>
2018-12-16 11:33:55 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { /*BrowserRouter*/ HashRouter as Router, Route, Switch } from 'react-router-dom';
 
+import PrivateRoute from './components/PrivateRoute/PrivateRoute'
+import { inject, observer } from 'mobx-react';
+
+import MainNav from './components/MainNav/MainNav'
+
+import HomePage from './pages/HomePage/HomePage'
+import SignupPage from './pages/SignupPage/SignupPage'
+import StatisticPage from './pages/StatisticPage/StatisticPage'
+import ContactPage from './pages/ContactPage/ContactPage'
+import ContactDetails from './pages/ContactDetails/ContactDetails'
+import ContactEdit from './pages/ContactEdit/ContactEdit'
+
+import './App.css'
+@inject('store')
+@observer
 class App extends Component {
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        <Router>
+          <div>
+            {this.props.store.userStore.user && <MainNav />}
+
+            <div className="app-content">
+              <Switch>
+                <Route path="/signup" component={SignupPage} />
+                <PrivateRoute path="/contacts/new" component={ContactEdit} />
+                <PrivateRoute path="/contacts/edit/:id?" component={ContactEdit} />
+                <PrivateRoute path="/contacts/:id" component={ContactDetails} />
+                <PrivateRoute path="/contacts" component={ContactPage} />
+                <PrivateRoute path="/statistics" component={StatisticPage} />
+                <PrivateRoute exact path="/" component={HomePage} />  
+              </Switch>
+            </div>
+          </div>
+        </Router>
       </div>
     );
   }
